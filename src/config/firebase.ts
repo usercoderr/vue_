907244d5dcd85974b8993835f30035630baab4bb
usercoderr <aidarali.prod@gmail.com>
2024-09-03 23:44:18 +0600

    change firebaseConfig

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyAQUnQyUruNUq5DtDbyV24kNPeGDi6whD0",
+    authDomain: "vueinterview-2ba48.firebaseapp.com",
+    projectId: "vueinterview-2ba48",
+    storageBucket: "vueinterview-2ba48.appspot.com",
+    messagingSenderId: "728825867771",
+    appId: "1:728825867771:web:f20fc58bfd2f0425d2ef5e"
 };
